78d25a82fb3d5c8e4f81e1469021d50aa6b838cb
AleenaNJ <aleenanj12@gmail.com>
2024-02-09 11:31:42 +0530

    security updated

--- controller/userRouter.js
+++ controller/userRouter.js
@@ -1,14 +1,32 @@
 const express=require("express")
 const usermodel=require("../model/usermodel")
+
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
 
 router.post("/add",async(req,res)=>{
-    let data=req.body
+    let {data}={"data":req.body}
+    let password=data.password
+hashPasswordGenerator(password).then(
+(hashedPassword)=>{
+    console.log(hashedPassword)
+    data.password=hashedPassword
+
+
+
+console.log(data)
+
     let user=new usermodel(data)
-    let result=await user.save()
+    let result=user.save()
     res.json({
         status:"success"
     })
+})
 })
 module.exports=router
